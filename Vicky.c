6d6197838d9da60d1f9902a5650cbadcf14e6106
vaidyaviviek1276 <143991388+vaidyaviviek1276@users.noreply.github.com>
2024-01-25 00:10:52 +0530

    Create Vicky.c

---
+++ Vicky.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float Maths,Eng,Sci,C,Linux,sum,pre,avg;
+    clrscr();
+    printf("Enter the marks of Maths:");
+    scanf("%f",&Maths)
+    printf("\nEnter the marks of Eng:");
+    scanf("%f",&Eng);
+    printf("\nEnter the marks of Sci");
+    scanf("%f",&Sci);
+    printf("\nEnter the marks of C:")
+    scanf("%f",&C);
+    printf("\nEnter the marks of Linux:");
+    scanf("%f",&Linux);
+
+
+sum=Maths+Eng+C+Linux;
+printf("Addition is %f",sum);
+avg=sum/500;
+printf("Average is %f",avg);
+pre=avg*100;
+printf("Percentage is %f",pre);
+getch();
+}
